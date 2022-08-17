7334f7cc2de45b632da5e7758f56bdbb1e77a947
sly <slydevwebproject@outlook.fr>
2022-08-18 00:38:40 +0200

    amélioration du code et style

--- app.js
+++ app.js
@@ -1,48 +1,57 @@
+let onScreen = document.querySelector(".display__screen");
+let minute = document.querySelector(".minute");
+let second = document.querySelector(".second");
+let btnStart = document.querySelector(".btn_start");
 
-function countDown(event) {
-    let currentBtn = event.target;
-    let onScreen = document.querySelector(".display__screen");
-    console.log(onScreen.innerText);
-    let minute = document.querySelector(".minute");
-    console.log(minute.innerText);
-    let second = document.querySelector(".second");
-    console.log(second.innerText);
-    let btnStart = document.querySelector(".btn_start");
-
-    if (btnStart.innerText != "START") {
-        minute.innerText = 25;
-        second.innerText = "0" + 0; 
-    } else {
-        btnStart.innerText = "RESET";
-        let interval = setInterval(function(){
-
-            second.innerText --;
-            console.log(second.innerText);
-            if (second.innerText.length < 2 && second.innerText < 10 && second.innerText > -1) {
-                second.innerText = "0" + second.innerText;
-            }
-            
-            if(second.innerText < 0) {
-                minute.innerText -= 1;
-                if (minute.innerText.length < 2 && minute.innerText < 10 && minute.innerText > -1) {
-                    minute.innerText = "0" + minute.innerText;
-                }
-                second.innerText = 59;
-            }
-            if (minute.innerHTML == 0 && second.innerText == 0) {
-                console.log("Driiiing");
-                alert("Driiiing");
-                clearInterval(interval);
-                second.innerText = "0" + 0;
-                minute.innerText = 25;
-                btnStart.innerText = "START";
-            }
-        },
-        1000);
-    }  
+function myStopFunction() {
+    clearInterval(interval);
 }
 
+let interval;
+
 let buttonStart = document.querySelector(".btn_start");
-buttonStart.addEventListener("click", countDown);
+buttonStart.addEventListener("click", () => {
+    clearInterval(interval);
+    if (btnStart.innerText === "START") {
+        btnStart.innerText = "PAUSE";
+        interval = setInterval(countDown, 1000);
+    } else {
+        console.log("sur pause");
+        clearInterval(interval);
+        btnStart.innerText = "START"
+    };
+});
 
+function countDown() {
+    second.innerText--;
+    console.log(second.innerText);
+    if (second.innerText.length < 2 && second.innerText < 10 && second.innerText > -1) {
+        second.innerText = "0" + second.innerText;
+    }
+    
+    if (second.innerText < 0) {
+        minute.innerText -= 1;
+        if (minute.innerText.length < 2 && minute.innerText < 10 && minute.innerText > -1) {
+            minute.innerText = "0" + minute.innerText;
+        }
+        second.innerText = 59;
+    }
+    if (minute.innerHTML == 0 && second.innerText == 0) {
+        console.log("Driiiing");
+        alert("Driiiing");
+        second.innerText = "0" + 0;
+        minute.innerText = 25;
+        btnStart.innerText = "START";
+        clearInterval(interval);
+    }
+}
 
+let buttonReset = document.querySelector(".btn_reset");
+buttonReset.addEventListener("click", reset);
+
+function reset() {
+    clearInterval(interval);
+    minute.innerText = 25;
+    second.innerText = "00";
+    btnStart.innerText = "START";
+}
